0f09daf3fcd8b220f0e06b67f74d2315bd79726d
teratti <c0b22106d3@edu.teu.ac.jp>
2023-06-20 14:12:51 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,6 @@
+import random
 import sys
 import pygame as pg
-
 
 WIDTH, HEIGHT = 1600, 900
 
@@ -11,8 +11,18 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bakudan = pg.Surface((20, 20))  #練習１
+    pg.draw.circle(bakudan,(255, 0, 0),(10, 10), 10) 
+    bakudan_x = random.randint(0, WIDTH)
+    bakudan_y = random.randint(0,HEIGHT) 
+    #爆弾surface(bakudan)から爆弾rectから抽出する 
+    bakudan_rct = bakudan.get_rect()
+    bakudan_rct.center = bakudan_x ,bakudan_y
+    bakudan.set_colorkey((0, 0, 0))
     clock = pg.time.Clock()
     tmr = 0
+    
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -20,6 +30,8 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bakudan,bakudan_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(10)
